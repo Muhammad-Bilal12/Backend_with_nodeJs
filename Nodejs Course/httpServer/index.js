7f4700f3a6598e3132afa54bd3f95f9f87ef8087
Muhammad Bilal <muhammadbilalmnaeem@gmail.com>
2022-10-23 22:41:57 +0500

    NodeJs web server routing

--- Nodejs Course/httpServer/index.js
+++ Nodejs Course/httpServer/index.js
@@ -22,13 +22,35 @@
 
 
 const http = require("http");
+const { type } = require("os");
 
 const server = http.createServer((req, res) => {
-    res.write('Hello World! from my side'); //write a response to the client
-    res.end(); //end the response
+    // console.log(req.url);
+
+    if (req.url == "/") {
+        // console.log(res.statusCode);
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write('<h1>Hello Home Page</h1>'); //write a response to the client
+        res.end(); //end the response
+    }
+    else if (req.url == "/about") {
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write('<h1>Hello About Page</h1>'); //write a response to the client
+        res.end(); //end the response
+    }
+    else if (req.url == "/contact") {
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write('<h1>Hello Contact Page</h1>'); //write a response to the client
+        res.end(); //end the response
+    } else {
+        res.writeHead(404, { "Content-type": "text/html" });
+        res.write('<h1>Error 404. Page Not Found</h1>'); //write a response to the client
+        res.end(); //end the response
+    }
+
 });
 
-server.listen("5000", () => {
+server.listen(5000, () => {
     console.log("The data listen from port 5000");
 });
 
