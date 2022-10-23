879a6ee58f8a6582cf70fbb35e072afbdfab8ea6
Muhammad Bilal <muhammadbilalmnaeem@gmail.com>
2022-10-23 21:43:29 +0500

    Module Wraper Function IIFE (some changes)

--- Nodejs Course/modWraperFun/index.js
+++ Nodejs Course/modWraperFun/index.js
@@ -1,7 +1,11 @@
 
-(function (exports, require, module, _filename, _dirname) {
-    const name = "Bilal";
-    console.log(name);
-})();
+// (function (exports, require, module, __filename, __dirname) {
+//     const name = "Bilal";
+//     console.log(name);
+// })();
 
 // console.log(name);
+
+// * you are not allowed to code like this
+console.log(__dirname);
+console.log(__filename);
