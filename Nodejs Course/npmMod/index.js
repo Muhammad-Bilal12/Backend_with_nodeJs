ae9c9494d7db7867a2d88b4614765a491e7999bc
Muhammad Bilal <muhammadbilalmnaeem@gmail.com>
2022-10-23 21:09:10 +0500

    NPM pkg in Nodejs

---
+++ Nodejs Course/npmMod/index.js
@@ -0,0 +1,14 @@
+const chalk = require("chalk");
+const validator = require("validator");
+
+
+// console.log(chalk.blue("Hello World!"));
+// console.log(chalk.blue.inverse("Hello World!"));
+// console.log(chalk.red.inverse("Error!"));
+// console.log(chalk.green.inverse("Success"));
+// console.log(chalk.yellow.inverse("Warn"));
+
+
+var res = validator.isEmail("bilal@gmail.com");
+
+console.log(res ? chalk.green.inverse(res) : chalk.red.inverse(res));
